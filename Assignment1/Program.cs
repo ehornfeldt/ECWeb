4c933e22d2e3e3a9a4476fd951b8baa22c230119
Elin Reckermann <elin89.ehr@gmail.com>
2024-11-27 15:25:49 +0100

    shopping list created

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,3 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿Console.WriteLine("Här kan du skapa din shoppinglista");
+Console.WriteLine("Skriv 'klar' när du är klar");
 
+List<string> products = [];
+while (true)
+{
+    Console.WriteLine("Ange din produkt:");
+    string product = Console.ReadLine()!;
+    if (product == "klar")
+    {
+        break;
+    }
+    products.Add(product);
+    Console.WriteLine(product, " tilllagd");
+}
+
+Console.WriteLine("Här är din lista:");
+
+foreach (string product in products)
+{
+    Console.WriteLine(product);
+}
+
+Console.ReadKey();
